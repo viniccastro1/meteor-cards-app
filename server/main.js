75003b0a5107da7ae4cb7addfdd785178b2f8c7a
Vinicius de Castro <78885142+viniccastro1@users.noreply.github.com>
2022-01-21 04:38:41 -0300

    Populated Meteor's db on startup

--- server/main.js
+++ server/main.js
@@ -5,4 +5,32 @@ import { CardsCollection } from "/imports/api/CardsCollection.js";
 import { CardsMethods } from "/imports/api/CardsMethods.js";
 import { CardsPublications } from "/imports/api/CardsPublications.js";
 
-Meteor.startup(() => {});
+// Those are sample cards so the application starts populated
+const insertCard = ({ name, url }) => {
+  CardsCollection.insert({ name: name, url: url });
+};
+
+const cardsSample = [
+  {
+    name: "Randall Holt",
+    url: "https://randomuser.me/api/portraits/men/85.jpg",
+  },
+  {
+    name: "Kristin Johnston",
+    url: "https://randomuser.me/api/portraits/women/49.jpg",
+  },
+  {
+    name: "Bertha Thomas",
+    url: "https://randomuser.me/api/portraits/women/48.jpg",
+  },
+  {
+    name: "Sean Burtton",
+    url: "https://randomuser.me/api/portraits/men/1.jpg",
+  },
+];
+
+Meteor.startup(() => {
+  if (CardsCollection.find().count() === 0) {
+    cardsSample.map((e) => insertCard({ name: e.name, url: e.url }));
+  }
+});
